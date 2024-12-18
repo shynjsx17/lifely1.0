c8e11eb31a844e204dad02ce192f3da10b5eb7db
John Ryley S. Maranan <johnryleysmaranan@iskolarngbayan.pup.edu.ph>
2024-12-18 22:14:20 +0800

    Add files via upload

--- Sidebar.jsx
+++ Sidebar.jsx
@@ -15,47 +15,49 @@ const Sidebar = () => {
 
   return (
     <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+      
       <div className="sidebar-header">
         <h3 className="brand">
-          <i className="fas fa-anchor"></i>
-          <span>MyApp</span>
+          <i className></i>
+          <span >Lifey</span>
         </h3>
         <div className="toggle-btn" onClick={toggleSidebar}>
           <i className={`fas ${isCollapsed ? 'fa-chevron-right' : 'fa-chevron-left'} toggle-icon`}></i>
         </div>
       </div>
       <ul className="nav-links">
+
         <li>
           <a href="#" className="nav-item">
-          <span className="nav-icon"><i className></i></span>
+          <span className="nav-icon"><img src="assets/icons8-home.svg" alt="Home Icon"/></span>
             <span>Home</span>
           </a>
         </li>
 
         <li>
           <a href="#" className="nav-item">
-            <span className="nav-icon"><i className></i></span>
+            <span className="nav-icon"><img src="assets/myday.svg" width="23px" alt="My Day Icon"/></span>
             <span>My Day</span>
           </a>
         </li>
 
         <li>
           <a href="#" className="nav-item">
-            <span className="nav-icon"><i className></i></span>
+            <span className="nav-icon"><img src="assets/calendar.svg" alt="My Calendar Icon"/></span>
             <span>My Calendar</span>
           </a>
         </li>
 
         <li>
           <a href="#" className="nav-item">
-            <span className="nav-icon"><i className></i></span>
+            <span className="nav-icon"><img src="assets/diary-svgrepo-com.svg" width="25px" alt="My Diary Icon"/></span>
             <span>My Diary</span>
           </a>
         </li>
 
         <li>
           <a href="#" className="nav-item">
-            <span className="nav-icon"><i className></i></span>
+            <span className="nav-icon"><img src="assets/archive.svg" alt="My Archive Icon"/></span>
             <span>My Archive</span>
           </a>
         </li>
@@ -63,7 +65,7 @@ const Sidebar = () => {
         <li className={`dropdown ${activeDropdown === 0 ? 'active' : ''}`}>
           <a href="#" className="nav-item dropdown-toggle" onClick={() => toggleDropdown(0)}>
             <div>
-              <span className="nav-icon"><i className></i></span>
+              <span className="nav-icon"><img src="assets/icons8-home.svg" alt="Home Icon"/></span>
               <span>My List</span>
             </div>
             <i className={`fas ${activeDropdown === 0 ? 'fa-chevron-down' : 'fa-chevron-right'} dropdown-icon`}></i>
