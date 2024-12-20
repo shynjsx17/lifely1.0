15cf3187781b3faa03447a33c3e332ea6fadbc77
Rouie01 <roxannefplandez@iskolarngbayan.pup.edu.ph>
2024-12-20 15:07:59 +0800

    Initial Commit -R

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'system-background': "url('./Images/BG.png')",
+      },
+      fontFamily: {
+        'poppins': ['poppins', 'san-serif']
+      },
+    },
   },
   plugins: [],
 }
