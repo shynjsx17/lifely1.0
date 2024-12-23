21315556b10f6d713b29299131fe3d942d32d1f5
august28 <llogbi124@gmail.com>
2024-12-24 00:41:28 +0800

    bye groupmates :> ⠀⠀⠀⠀⠀⠀⢀⣤⠶⠟⠛⠛⠛⠶⢦⡀⠀⠀⠀⠀⣀⣀⣀⡀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⢠⣾⠉⠀⠀⠀⠀⠀⠀⠀⠹⣆⢀⣴⠟⠋⠉⠉⠛⠳⣦⡀⠀⠀ ⠀⠀⠀⠀⠀⣾⠇⠀⠀⠀⠀⢀⣤⣤⣄⠀⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⢻⡆⠀ ⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠸⣿⣿⣿⠀⣿⠃⢰⣿⣿⣷⠀⠀⠀⠀⠈⣿⠀ ⠀⠀⠀⠀⠀⣸⣧⠀⠀⠀⠀⠀⠉⠉⢁⣴⣿⡀⠈⠻⠿⠋⠀⠀⠀⠀⢰⡟⠀ ⠀⠀⠀⢀⣼⠟⠙⠷⣦⣄⣀⣀⣤⣶⠟⠁⠈⢷⣄⠀⠀⠀⠀⠀⢀⣠⣾⠁⠀ ⠀⠀⠀⣼⡏⡐⠈⠀⠑⡍⠉⠉⠁⠀⠀⠀⠀⠀⠉⠛⠷⠶⠶⠟⠛⠉⢻⣇⠀ ⠀⠀⠀⣿⠁⠆⠀⠀⢀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠖⠁⠀⠑⡀⢿⠀ ⠀⠀⠀⢹⡆⠀⠁⠀⠁⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢄⠀⠀⢀⠃⣼⠇ ⠀⠀⣾⡿⢿⣦⣀⠀⠀⠀⠙⢶⣄⠀⠀⠀⠀⠀⣀⣤⡶⠗⠀⠁⠈⠀⣰⡟⠀ ⢀⣹⡏⠁⠀⠈⠉⣻⣶⣦⣤⣄⣉⣳⣦⣠⣴⠿⠋⠁⠀⠀⢀⣀⣤⣾⠟⠀⠀ ⠘⢿⣄⡀⠀⠀⣰⡟⠁⠀⠀⢉⣭⣟⡛⣛⣻⡛⠛⠛⠻⣿⠿⠿⣭⣤⡀⠀⠀ ⠀⠀⣨⣿⣷⣾⠿⣆⣠⡶⣦⢸⣿⣿⠿⣿⣿⡇⠀⠀⠀⠹⣆⠀⠀⢈⣹⡆⠀ ⠀⠐⡏⠀⠈⠁⠀⠈⠋⠀⣸⡆⠁⠀⠀⠈⠉⠀⠀⠀⠀⠀⢹⡆⣀⣼⠇⠀⠀ ⠀⠀⢷⡀⠀⠀⠀⠀⠀⣠⡿⢦⣤⣤⣤⣀⣀⣀⣀⣀⣀⣀⣀⣿⠛⠁⠀⠀⠀ ⠀⠀⠀⠙⠶⠤⣤⠴⠾⠋⠀⠀⠀⢸⡇⠀⣉⣍⠉⠉⢉⡉⠉⠹⣆⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⠞⠛⠉⠳⠶⠟⠙⠓⠾⠋⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀

--- backend/index.php
+++ backend/index.php
@@ -8,29 +8,24 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
 
-// Include core files
 require_once './config/config.php';
 require_once './config/db_connect.php';
 
 $database = new Database();
-$db = $database->connect(); // Changed method
+$db = $database->connect(); 
 
-// Get the request URI and method
 $request = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
-// Parse the URI to get the endpoint
 $uri = parse_url($request, PHP_URL_PATH);
 $uri = explode('/', $uri);
 $endpoint = end($uri);
 
-// Handle preflight requests
 if ($method === "OPTIONS") {
     header("HTTP/1.1 200 OK");
     exit();
 }
 
-// Route to appropriate handler
 switch ($endpoint) {
     case 'login':
         require_once './api/login.php';
